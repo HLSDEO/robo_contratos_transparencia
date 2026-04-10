292efd0041e0c24186088a28d890cfa5f0b99ca8
Helison Lucas <helisond97@gmail.com>
2026-04-10 12:41:13 -0300

    Correção prazo do link

--- config/parametros.py
+++ config/parametros.py
@@ -1,4 +1,4 @@
 #SITE = "https://contratos.comprasnet.gov.br/transparencia/contratos?orgao=%5B%2230108%22%5D&vigencia_fim=%7B%22from%22%3A%222026-02-20%22%2C%22to%22%3A%222040-02-25%22%7D&unidade={UNIDADE}"
-SITE = "https://contratos.comprasnet.gov.br/transparencia/contratos?orgao=%5B%2230108%22%5D&vigencia_fim=%7B%22from%22%3A%222026-02-20%22%2C%22to%22%3A%222040-02-25%22%7D&vigencia_inicio=%7B%22from%22%3A%222021-01-01%22%2C%22to%22%3A%222024-01-01%22%7D&unidade={UNIDADE}"
+SITE = "https://contratos.comprasnet.gov.br/transparencia/contratos?orgao=%5B%2230108%22%5D&unidade={UNIDADE}&vigencia_fim=%7B%22from%22%3A%222026-02-20%22%2C%22to%22%3A%222040-02-25%22%7D&vigencia_inicio=%7B%22from%22%3A%222021-01-01%22%2C%22to%22%3A%222040-01-01%22%7D"
 TEMPO_ESPERA = 5 # em segundos
 AMBIENTE = "prod" # dev #prod
